5ce933f36b2d50094a8010892599c9eddaa65d9a
AmakoveBridgit <amakovebridgit@gmail.com>
2023-09-06 13:36:39 +0300

    Corrected failing tests

--- etaka/src/App.test.js
+++ etaka/src/App.test.js
@@ -1,9 +1,9 @@
 import { render} from '@testing-library/react';
 import App from './App';
 
-it('displays the About component', () => {
+it('displays the Services component', () => {
   const { getByText } = render(<App />);
-  const aboutHeading = getByText('About Us', { exact: false }); // Use { exact: false } for partial text matching
+  const aboutHeading = getByText('Services', { exact: false }); // Use { exact: false } for partial text matching
   expect(aboutHeading).toBeInTheDocument();
 });
 
